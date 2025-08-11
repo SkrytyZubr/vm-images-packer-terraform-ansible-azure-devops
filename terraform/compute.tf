e03d870365e4d716562f93b4c07d171568c88f44
SkrytyZubr <michal.czarnecki03@gmail.com>
2025-08-12 00:19:06 +0200

    create compute.tf

---
+++ terraform/compute.tf
@@ -0,0 +1,52 @@
+resource "azurerm_storage_account" "stor" {
+  name = "bookstore2"
+  location = var.location
+  resource_group_name = var.resource_group_name
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+}
+
+data "azurerm_image" "customnginx" {
+  name = "linuxWeb-v0.0.1"
+  resource_group_name = "rg_images"
+}
+
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "bookvm"
+  location              = "West Europe"
+  resource_group_name   = azurerm_resource_group.rg.name
+  vm_size               = "Standard_DS2_v2"
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  ## USE THE CUSTOM IMAGE
+  storage_image_reference {
+    id = data.azurerm_image.customngnix.id
+  }
+
+
+  storage_os_disk {
+    name              = "book-osdisk"
+    managed_disk_type = "Standard_LRS"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+  }
+
+  os_profile {
+    computer_name  = "VMBOOK"
+    admin_username = "book"
+    admin_password = "book123*"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  boot_diagnostics {
+    enabled     = true
+    storage_uri = azurerm_storage_account.stor.primary_blob_endpoint
+  }
+
+  tags = {
+    "role" = "webserver"
+  }
+}
--- terraform/network.tf
+++ terraform/network.tf
@@ -11,3 +11,59 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name = azurerm_resource_group.rg.name
   address_prefixes = [ "10.0.10.0/24" ]
 }
+
+resource "azurerm_public_ip" "pip" {
+  name = "book-pip"
+  location = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method = "Static"
+  domain_name_label = "bookmcz"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name = "book-nic"
+  location = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name = "bookipconfig"
+    subnet_id = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "book-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nic_nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
