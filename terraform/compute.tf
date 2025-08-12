a9584ff449a08e744c02617a888f3729244bb4bf
SkrytyZubr <michal.czarnecki03@gmail.com>
2025-08-12 14:24:09 +0200

    fixed names

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -1,7 +1,7 @@
 resource "azurerm_storage_account" "stor" {
   name = "bookstore2mc"
   location = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.rg.name
   account_tier = "Standard"
   account_replication_type = "LRS"
 }
